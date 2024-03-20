9d9806de6c95991878a14866b158b2d1ed1a2452
fiadh <fiadh@byu.edu>
2024-03-20 12:40:34 -0600

    serverFacade

---
+++ client/src/main/java/facade/ServerFacade.java
@@ -0,0 +1,123 @@
+package facade;
+
+import chess.ChessGame;
+import com.google.gson.Gson;
+import requests.*;
+import responses.AuthResponse;
+import responses.BaseResponse;
+import responses.CreateGameResponse;
+import responses.ListGamesResponse;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class ServerFacade {
+    private final String baseUrl;
+
+    public ServerFacade(String baseUrl) {
+        this.baseUrl = baseUrl;
+    }
+
+
+    public AuthResponse registerUser(RegisterRequest request) throws IOException {
+        String username = request.getUsername();
+        String password = request.getPassword();
+        String email = request.getEmail();
+
+        String endpoint = baseUrl + "/user";
+
+        String requestBody = String.format("{\"username\": \"%s\", \"password\": \"%s\", \"email\": \"%s\"}", username, password, email);
+
+        return sendRequest("POST", endpoint, requestBody, null, AuthResponse.class);
+    }
+
+    public AuthResponse loginUser(LoginRequest request) throws IOException {
+        String username = request.getUsername();
+        String password = request.getPassword();
+
+        String endpoint = baseUrl + "/session";
+
+        String requestBody = String.format("{\"username\": \"%s\", \"password\": \"%s\"}", username, password);
+
+        return sendRequest("POST", endpoint, requestBody, null, AuthResponse.class);
+    }
+
+    public BaseResponse logoutUser(BaseRequest request, String authToken) throws IOException {
+
+        String endpoint = baseUrl + "/session";
+
+        return sendRequest("DELETE", endpoint, null, authToken, BaseResponse.class);
+    }
+
+    public CreateGameResponse createGame(CreateGameRequest request, String authToken) throws IOException {
+        String gameName = request.getGameName();
+
+        String endpoint = baseUrl + "/game";
+
+        String requestBody = String.format("{\"gameName\": \"%s\"}", gameName);
+
+        return sendRequest("POST", endpoint, requestBody, authToken, CreateGameResponse.class);
+    }
+
+    public BaseResponse joinGame(JoinGameRequest request, String authToken) throws IOException {
+        ChessGame.TeamColor playerColor = request.getPlayerColor();
+        Integer gameID = request.getGameID();
+
+        String endpoint = baseUrl + "/game";
+
+        String requestBody = String.format("{\"gameID\": %d, \"playerColor\": \"%s\"}", gameID, playerColor);
+
+        return sendRequest("PUT", endpoint, requestBody, authToken, BaseResponse.class);
+    }
+
+    public ListGamesResponse listGames(BaseRequest request, String authToken) throws IOException {
+        String endpoint = baseUrl + "/game";
+
+        return sendRequest("GET", endpoint, null, authToken, ListGamesResponse.class);
+    }
+
+    public BaseResponse clearDatabase(BaseRequest request) throws IOException {
+        String endpoint = baseUrl + "/db";
+
+        return sendRequest("DELETE", endpoint, null, null, BaseResponse.class);
+    }
+
+
+    private <T> T sendRequest(String method, String endpoint, String requestBody, String authToken, Class<T> responseClass) throws IOException {
+        URL url = new URL(endpoint);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod(method);
+        connection.setConnectTimeout(5000);
+        connection.setReadTimeout(5000);
+        connection.setDoOutput(true);
+
+        if (authToken != null) {
+            connection.setRequestProperty("Authorization", "Bearer " + authToken);
+        }
+
+        if (requestBody != null) {
+            try (OutputStream os = connection.getOutputStream()) {
+                os.write(requestBody.getBytes());
+            }
+        }
+
+        int responseCode = connection.getResponseCode();
+        InputStream responseBodyStream = null;
+
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+            responseBodyStream = connection.getInputStream();
+        } else {
+            responseBodyStream = connection.getErrorStream();
+        }
+
+        InputStreamReader responseBodyReader = new InputStreamReader(responseBodyStream);
+        T responseObject = new Gson().fromJson(responseBodyReader, responseClass);
+
+        responseBodyReader.close();
+        connection.disconnect();
+
+        return responseObject;
+    }
+
+}
--- client/src/test/java/clientTests/ServerFacadeTests.java
+++ client/src/test/java/clientTests/ServerFacadeTests.java
@@ -1,18 +1,32 @@
 package clientTests;
 
+import facade.ServerFacade;
 import org.junit.jupiter.api.*;
+import requests.RegisterRequest;
+import responses.AuthResponse;
 import server.Server;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class ServerFacadeTests {
 
     private static Server server;
+    private static ServerFacade facade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+        facade = new ServerFacade("http://localhost:" + port);
+    }
+
+    @BeforeEach
+    void clearDatabase() throws IOException {
+        facade.clearDatabase(null);
     }
 
     @AfterAll
@@ -26,4 +40,80 @@ public class ServerFacadeTests {
         Assertions.assertTrue(true);
     }
 
-}
+    @Test
+    void registerUser_Positive() throws IOException {
+        RegisterRequest request = new RegisterRequest("username", "password", "email@example.com");
+        AuthResponse response = facade.registerUser(request);
+
+        assertEquals("username", response.getUsername());
+        assertNotNull(response.getAuthToken());
+        assertNull(response.getMessage());
+    }
+    @Test
+    void registerUser_Negative() throws IOException {
+        RegisterRequest request = new RegisterRequest("username", "password", "email@example.com");
+        AuthResponse response = facade.registerUser(request);
+        AuthResponse responseDuplicate = facade.registerUser(request);
+
+        assertNull((responseDuplicate.getUsername()));
+        assertNull((responseDuplicate.getAuthToken()));
+        assertNotNull(responseDuplicate.getMessage());
+    }
+
+    @Test
+    void loginUser_Positive() throws IOException {
+
+    }
+    @Test
+    void loginUser_Negative() throws IOException {
+
+    }
+
+    @Test
+    void logoutUser_Positive() throws IOException {
+
+    }
+    @Test
+    void logoutUser_Negative() throws IOException {
+
+    }
+
+    @Test
+    void createGame_Positive() throws IOException {
+
+    }
+    @Test
+    void createGame_Negative() throws IOException {
+
+    }
+
+    @Test
+    void joinGame_Positive() throws IOException {
+
+    }
+    @Test
+    void joinGame_Negative() throws IOException {
+
+    }
+
+    @Test
+    void listGames_Positive() throws IOException {
+
+    }
+    @Test
+    void listGames_Negative() throws IOException {
+
+    }
+
+    @Test
+    void clearDatabase_Positive() throws IOException {
+
+    }
+
+
+
+
+
+
+
+    }
