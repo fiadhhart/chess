390d9fee798785bf4768669c3b1b055be3196ea6
fiadh <fiadh@byu.edu>
2024-04-11 21:06:21 -0600

    cleanup

--- shared/src/main/java/responses/AuthResponse.java
+++ shared/src/main/java/responses/AuthResponse.java
@@ -20,9 +20,6 @@ public class AuthResponse extends BaseResponse{
     public String getUsername() {
         return username;
     }
-    public void setUsername(String username) {
-        this.username = username;
-    }
     public String getAuthToken() {
         return authToken;
     }
--- shared/src/main/java/responses/GameResponse.java
+++ shared/src/main/java/responses/GameResponse.java
@@ -24,9 +24,6 @@ public class GameResponse extends BaseResponse{
     public Integer getGameID() {
         return gameID;
     }
-    public void setGameID(Integer gameID) {
-        this.gameID = gameID;
-    }
     public String getWhiteUsername() {
         return whiteUsername;
     }
